7a5e871994a9930c9e9032f8586a6741623b7b70
zt <lzttzllzttzl@gmail.com>
2025-10-09 17:19:01 +0800

    Add COMMAND_WORD variable to command class

--- src/main/java/seedu/address/logic/commands/Command.java
+++ src/main/java/seedu/address/logic/commands/Command.java
@@ -7,7 +7,7 @@ import seedu.address.model.Model;
  * Represents a command with hidden internal logic and the ability to be executed.
  */
 public abstract class Command {
-
+    public static final String COMMAND_WORD = "command";
     /**
      * Executes the command and returns the result message.
      *
