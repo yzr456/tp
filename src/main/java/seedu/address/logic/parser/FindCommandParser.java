e987dc118dbb3935a56ce3162196dd50cea091e4
Zirui <97959216+yzr456@users.noreply.github.com>
2025-10-15 20:02:15 +0800

    Update logic in FindCommand

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -26,8 +26,7 @@ public class FindCommandParser implements Parser<FindCommand> {
 
         String[] nameKeywords = trimmedArgs.split("\\s+");
 
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)),
-                Arrays.asList(nameKeywords));
+        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
     }
 
 }
