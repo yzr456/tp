c275d00f22c72e85409d925fc3116495f69572b0
zt <lzttzllzttzl@gmail.com>
2025-10-15 19:05:48 +0800

    Update AddCommandParser to support StudyYear class

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -13,11 +13,7 @@ import java.util.stream.Stream;
 
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
+import seedu.address.model.person.*;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -43,7 +39,7 @@ public class AddCommandParser implements Parser<AddCommand> {
         argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_STUDY_YEAR,
                 PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS);
         Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
-        String studyYear = argMultimap.getValue(PREFIX_STUDY_YEAR).get();
+        StudyYear studyYear = ParserUtil.parseStudyYear(argMultimap.getValue(PREFIX_STUDY_YEAR).get());
         Phone phone = ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE).get());
         Email email = ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get());
         Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get());
