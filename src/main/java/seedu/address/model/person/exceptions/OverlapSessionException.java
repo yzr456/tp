7bf0dfda4c6c1a0b5e8eecfedbe4534955d69adf
zt <lzttzllzttzl@gmail.com>
2025-10-23 20:22:12 +0800

    Add JavaDoc comments

--- src/main/java/seedu/address/model/person/exceptions/OverlapSessionException.java
+++ src/main/java/seedu/address/model/person/exceptions/OverlapSessionException.java
@@ -1,6 +1,14 @@
 package seedu.address.model.person.exceptions;
 
+/**
+ * Signals that the operation will result in overlapping sessions.
+ * This exception is thrown when attempting to add a session that conflicts with an existing session
+ * in the weekly schedule.
+ */
 public class OverlapSessionException extends RuntimeException {
+    /**
+     * Constructs an OverlapSessionException with a default error message.
+     */
     public OverlapSessionException() {
         super("Operation would result in an overlapping session");
     }
