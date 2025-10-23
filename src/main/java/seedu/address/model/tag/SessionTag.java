b8f2dcf8ecb9364d66af267b4cfb2b0b598bec94
zt <lzttzllzttzl@gmail.com>
2025-10-23 15:14:36 +0800

    Update SessionTag to override methods

--- src/main/java/seedu/address/model/tag/SessionTag.java
+++ src/main/java/seedu/address/model/tag/SessionTag.java
@@ -1,7 +1,40 @@
 package seedu.address.model.tag;
 
+import seedu.address.model.person.Session;
+
 public class SessionTag extends Tag {
-    public SessionTag(String tagName) {
+    private final Session session;
+
+    public SessionTag(String tagName, Session session) {
         super(tagName);
+        this.session = session;
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    @Override
+    public boolean isSessionTag() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof SessionTag)) {
+            return false;
+        }
+
+        SessionTag otherTag = (SessionTag) other;
+        return tagName.equals(otherTag.tagName) && session.equals(otherTag.getSession());
+    }
+
+    @Override
+    public int hashCode() {
+        return tagName.hashCode() + session.hashCode();
     }
 }
