9a206e79bdb7a0882fba82d515f8c5d3ced1fc17
yzr456 <97959216+yzr456@users.noreply.github.com>
2025-10-27 11:22:22 +0800

    Update Messages.java

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -12,14 +12,14 @@ import seedu.address.model.person.Person;
  */
 public class Messages {
 
-    public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
+    public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command.";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "Invalid index number "
             + "provided. Index must be a single positive integer within the contact list range.\n";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d contact(s) found for search %2$s";
     public static final String MESSAGE_DUPLICATE_FIELDS =
                 "Multiple values specified for the following single-valued field(s): ";
-    public static final String MESSAGE_MISSING_INDEX = "The index field cannot be blank";
+    public static final String MESSAGE_MISSING_INDEX = "The index field cannot be blank.";
     public static final String MESSAGE_MISSING_PREFIX =
             "Missing prefix(s) for parameter(s). Ensure that your input follows this format: \n%1$s";
 
