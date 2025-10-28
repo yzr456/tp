a0973c9525123345ceeac131edafb749b5a7814c
zt <lzttzllzttzl@gmail.com>
2025-10-28 15:09:26 +0800

    Update message format

--- src/main/java/seedu/address/model/person/StudyYear.java
+++ src/main/java/seedu/address/model/person/StudyYear.java
@@ -9,14 +9,14 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class StudyYear {
     public static final String MESSAGE_CONSTRAINTS =
-            "Study year should be of format {ACAD_LEVEL}{NUMBER} "
-            + "and adhere to the following constraints:\n"
-            + "ACAD_LEVEL           NUMBER(range)\n"
-            + " PRI                         1 - 6\n"
-            + " SEC                         1 - 5\n"
-            + " JC                          1 - 2\n"
-            + " POLY                        1 - 3\n"
-            + " UNI                         1 - 5";
+            "Study year should be of format [ACAD_LEVEL][NUMBER]\n"
+                    + "and adhere to the following constraints:\n"
+                    + String.format("%-15s %s\n", "ACAD_LEVEL", "NUMBER(range)")
+                    + String.format("%-15s %s\n", "PRI", "1 - 6")
+                    + String.format("%-15s %s\n", "SEC", "1 - 5")
+                    + String.format("%-15s %s\n", "JC", "1 - 2")
+                    + String.format("%-15s %s\n", "POLY", "1 - 3")
+                    + String.format("%-15s %s", "UNI", "1 - 5");
 
     public static final String VALIDATION_REGEX = "^(PRI[1-6]|SEC[1-5]|JC[1-2]|POLY[1-3]|UNI[1-5])$";
     public final String value;
