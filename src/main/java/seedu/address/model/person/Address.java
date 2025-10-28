1d82c445be5eee72d781ea6c26c0b305d51372a0
TeeJinSeng <teejinsengs@gmail.com>
2025-10-29 01:08:34 +0800

    Update maximum length for `Email` and `isValidate...()` methods for other models

--- src/main/java/seedu/address/model/person/Address.java
+++ src/main/java/seedu/address/model/person/Address.java
@@ -11,12 +11,12 @@ public class Address {
 
     public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, and it should not be blank";
 
-    public static final int MAXIMUM_LENGTH_ADDRESS = 70;
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "^(?=.{1," + MAXIMUM_LENGTH_ADDRESS + "}$)[^\\s].*$";
+    public static final String VALIDATION_REGEX = "[^\\s].*";
+    public static final int MAXIMUM_LENGTH_ADDRESS = 70;
 
     public final String value;
 
@@ -35,7 +35,7 @@ public class Address {
      * Returns true if a given string is a valid email.
      */
     public static boolean isValidAddress(String test) {
-        return test.matches(VALIDATION_REGEX);
+        return test.length() <= MAXIMUM_LENGTH_ADDRESS & test.matches(VALIDATION_REGEX);
     }
 
     @Override
--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -12,13 +12,12 @@ public class Name {
     public static final String MESSAGE_CONSTRAINTS =
             "Names should only contain alphanumeric characters and spaces, and it should not be blank";
 
-    public static final int MAXIMUM_LENGTH_NAME = 30;
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX =
-            "^(?=.{1," + MAXIMUM_LENGTH_NAME + "}$)[\\p{Alnum} '-]*[\\p{Alnum}][\\p{Alnum} '-]*$";
+    public static final String VALIDATION_REGEX = "[\\p{Alnum} '-]*[\\p{Alnum}][\\p{Alnum} '-]*";
+    public static final int MAXIMUM_LENGTH_NAME = 30;
 
     public final String fullName;
 
@@ -37,7 +36,7 @@ public class Name {
      * Returns true if a given string is a valid name.
      */
     public static boolean isValidName(String test) {
-        return test.matches(VALIDATION_REGEX);
+        return test.length() <= MAXIMUM_LENGTH_NAME & test.matches(VALIDATION_REGEX);
     }
 
 
